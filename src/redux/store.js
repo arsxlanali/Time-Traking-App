59fbaf532e48f802d047c779a3c4dcee17250aee
marsad11223 <marsad11223@gmail.com>
2022-08-31 18:59:02 +0500

    finalize changes

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,7 @@ import slideBarSliceReducer from "./Slice/sideBarSlice";
 import viewEmployees from "./Slice/employeesSlice";
 import employee from "./Slice/employeeSllice";
 import loginSliceReducer from "./Slice/loginSlice";
-import viewTimeSheetReducer from "./Slice/loginSlice";
+import viewTimeSheetReducer from "./Slice/viewTimeSheetSlice";
 export default configureStore({
   reducer: {
     slideBar: slideBarSliceReducer,
