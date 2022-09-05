4c4f4e87b6fc8d4ce9733a9a69e0928b1ab62492
jawadakram00 <jawadakram.tdc@gmail.com>
2022-09-05 17:15:53 +0500

    store commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import slideBarSliceReducer from "./Slice/sideBarSlice";
 import viewEmployees from "./Slice/employeesSlice";
-import employee from "./Slice/employeeSllice";
 import loginSliceReducer from "./Slice/loginSlice";
 import viewTimeSheetReducer from "./Slice/viewTimeSheetSlice";
 import viewProjectsReducer from './Slice/projectSlice';
@@ -9,7 +8,6 @@ export default configureStore({
   reducer: {
     slideBar: slideBarSliceReducer,
     employees: viewEmployees,
-    employee: employee,
     login: loginSliceReducer,
     viewTimeSheet: viewTimeSheetReducer,
     viewProjects:viewProjectsReducer
