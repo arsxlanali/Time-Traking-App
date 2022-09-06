de043868bf69377fc4d2ac6a6e75c60629d1566b
marsad11223 <marsad11223@gmail.com>
2022-09-06 12:56:08 +0500

    Added the formik in login screen

--- src/views/pages/loginTimeTrack/LoginApp.js
+++ src/views/pages/loginTimeTrack/LoginApp.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { login } from "src/redux/Slice/loginSlice";
-import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router-dom";
+// import { useDispatch, useSelector } from "react-redux";
+// import { useHistory } from "react-router-dom";
 
 import {
   CButton,
@@ -22,106 +22,174 @@ import {
   CRow,
 } from "@coreui/react";
 import CIcon from "@coreui/icons-react";
+import {
+  CInvalidFeedback,
+  CInputCheckbox,
+  CFormGroup,
+  CLabel,
+} from "@coreui/react";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import { useHistory, useLocation } from "react-router-dom/cjs/react-router-dom";
+// import Toaster from "src/views/notifications/toaster/Toaster";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+// import { resetPassword } from "src/redux/Slice/employeesSlice"
+// import loginSlice from "src/redux/Slice/loginSlice";
+// import { login } from "src/redux/Slice/loginSlice";
+const validationSchema = function (values) {
+  return Yup.object().shape({
+    email: Yup.string()
+      .email("Invalid email address")
+      .required("Email is required!"),
+    password: Yup.string()
+      .min(6, `Password has to be at least ${6} characters!`)
+      .matches(
+        /(?=.*\d)(?=.*[a-z]).{6,}/,
+        "Password must contain: numbers, uppercase and lowercase letters\n"
+      )
+      .required("Password is required")
+  });
+};
+
+const validate = (getValidationSchema) => {
+  return (values) => {
+    const validationSchema = getValidationSchema(values);
+    try {
+      validationSchema.validateSync(values, { abortEarly: false });
+      return {};
+    } catch (error) {
+      return getErrorsFromValidationError(error);
+    }
+  };
+};
+
+const getErrorsFromValidationError = (validationError) => {
+  const FIRST_ERROR = 0;
+  return validationError.inner.reduce((errors, error) => {
+    return {
+      ...errors,
+      [error.path]: error.errors[FIRST_ERROR],
+    };
+  }, {});
+};
 
 function LoginApp() {
-  const history = useHistory();
-
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
-
-  function handleEmail(event) {
-    setEmail(event.target.value);
-  }
-
-  function handlePas(event) {
-    setPassword(event.target.value);
-  }
-
+  const { isLoading, isScuessfull } = useSelector((state) => state.login);
   const dispatch = useDispatch();
-  const { entities, loading } = useSelector((state) => state.login);
-
-  if (loading) return <p>Loading...</p>;
-
-  
-  const navigate = () => {
-    history.push("/dashboard");
+  const history = useHistory();
+  const onSubmit = (values, setSubmitting) => {
+    // values["id"] = user._id;
+    dispatch(login(values));
+    setSubmitting(isLoading);
   };
-
-  const onSubmit = () => {
-    let data = {
-      email,
-      password,
-    };
-    dispatch(login({ data, navigate }));
+  // console.log("this is issuccessfful", isScuessfull)
+  if (isScuessfull) {
+    setTimeout(() => history.push(`/dashboard`), 5000);
+    // return (
+    //   <div>
+    //     <Toaster></Toaster>
+    //   </div>
+    // );
+  }
+  const initialValues = {
+    email: "",
+    password: "",
   };
-
-
 
 
   return (
-      <div className="c-app c-default-layout flex-row align-items-center">
-        <CContainer>
-          <CRow className="justify-content-center">
-            <CCol md="8">
-              <CCardGroup>
-                <CCard className="p-4">
-                  <CCardBody>
-                    <CForm>
-                      <h1>Login</h1>
-                      <p className="text-muted">Sign In to your account</p>
-                      <CInputGroup className="mb-3">
-                        <CInputGroupPrepend>
-                          <CInputGroupText>
-                            <CIcon name="cil-user" />
-                          </CInputGroupText>
-                        </CInputGroupPrepend>
-                        <CInput
-                          type="text"
-                          placeholder="Email"
-                          autoComplete="email"
-                          onChange={handleEmail}
-                          required
+    <div className="c-app c-default-layout flex-row align-items-center">
+      <CContainer>
+        <CRow className="justify-content-center">
+          <CCol md="8">
+            <CCard className="p-4">
+              <Formik
+                initialValues={initialValues}
+                validate={validate(validationSchema)}
+                onSubmit={onSubmit}
+              >
+                {({
+                  values,
+                  errors,
+                  touched,
+                  handleChange,
+                  handleBlur,
+                  handleSubmit,
+                  isLoading,
+                  isValid,
+                }) => (
+                  <CRow>
+                    <CCol>
+                      <CForm onSubmit={handleSubmit} noValidate name="simpleForm1">
+                        <h1>Login</h1>
+                        <p className="text-muted">Sign In to your account</p>
+                        {/* <CCol> */}
+                        <CFormGroup>
+                          <CLabel htmlFor="password">Password</CLabel>
+                          <CInput
+                            type="password"
+                            name="password"
+                            id="password"
+                            placeholder="Password"
+                            autoComplete="new-password"
+                            valid={!errors.password}
+                            invalid={touched.password && !!errors.password}
+                            required
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.password}
+                          />
+                          <CInvalidFeedback>{errors.password}</CInvalidFeedback>
+                        </CFormGroup>
+                        {/* </CCol> */}
+                        {/* <CCol> */}
+                        <CFormGroup>
+                          <CLabel htmlFor="email">Email</CLabel>
+                          <CInput
+                            type="email"
+                            name="email"
+                            id="email"
+                            placeholder="Email"
+                            autoComplete="email"
+                            valid={!errors.email}
+                            invalid={touched.email && !!errors.email}
+                            required
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.email}
+                          />
+                          <CInvalidFeedback>{errors.email}</CInvalidFeedback>
+                        </CFormGroup>
+                        {/* </CCol> */}
 
-                        />
-                      </CInputGroup>
-                      <CInputGroup className="mb-4">
-                        <CInputGroupPrepend>
-                          <CInputGroupText>
-                            <CIcon name="cil-lock-locked" />
-                          </CInputGroupText>
-                        </CInputGroupPrepend>
-                        <CInput
-                          type="password"
-                          placeholder="Password"
-                          autoComplete="current-password"
-                          onChange={handlePas}
-                          required
-                        />
-                      </CInputGroup>
-                      <CRow>
-                        <CCol xs="6">
-                          <CButton
-                            color="primary"
-                            className="px-4"
-                            onClick={onSubmit}
-                          >
-                            Login
-                          </CButton>
-                        </CCol>
-                        <CCol xs="6" className="text-right">
-                          <CButton color="link" className="px-0">
-                            Forgot password?
-                          </CButton>
-                        </CCol>
-                      </CRow>
-                    </CForm>
-                  </CCardBody>
-                </CCard>
-              </CCardGroup>
-            </CCol>
-          </CRow>
-        </CContainer>
-      </div>
+                        {/* <CFormGroup> */}
+                        <CRow>
+                          <CCol xs="6">
+                            <CButton
+                              type="submit"
+                              color="primary"
+                              className="mr-1"
+                              disabled={isLoading || !isValid}
+                            >
+                              {isLoading ? "Wait..." : "Login"}
+                            </CButton>
+                          </CCol>
+                          <CCol xs="6" className="text-right">
+                            <CButton color="link" className="px-0">Forgot password?</CButton>
+                          </CCol>
+                        </CRow>
+                        {/* </CFormGroup> */}
+                      </CForm>
+                    </CCol>
+                  </CRow>
+                )}
+              </Formik>
+            </CCard>
+          </CCol>
+        </CRow>
+      </CContainer>
+    </div>
   );
 }
 
