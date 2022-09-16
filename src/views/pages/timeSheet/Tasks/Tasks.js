4562aee0dd83a55c573794cec966ffc309edc0ca
marsad11223 <marsad11223@gmail.com>
2022-09-16 16:11:43 +0500

    this is adding task types

--- src/views/pages/timeSheet/Tasks/Tasks.js
+++ src/views/pages/timeSheet/Tasks/Tasks.js
@@ -1,24 +1,27 @@
 const _tasks = {
-    DEVELOPMENT: [{ value: 'General', label: 'General' },
-    { value: 'Development', label: 'Development' },
-    { value: 'Testing', label: 'Testing' },
-    { value: 'Meeting', label: 'Meeting' },
-    { value: 'Leading', label: 'Leading' },
-    { value: 'Learning', label: 'Learning' },
-    { value: 'Traning', label: 'Traning' },
-    { value: 'Code Review', label: 'Code Review' },
-    { value: 'Code Analysis', label: 'Code Analysis' },
-    { value: 'Code Analysis', label: 'Code Analysis' },
-    { value: 'Code Refactoring', label: 'Code Refactoring' },
-    { value: 'Session', label: 'Session' },
-    { value: 'Discussion', label: 'Discussion' },
-    { value: 'Debugging', label: 'Debugging' },
-    { value: 'Reporting', label: 'Reporting' },
-    { value: 'Writing Docs', label: 'Writing Docs' },
-    { value: 'Designing', label: 'Designing' },
-    { value: 'Project Setup', label: 'Project Setup' },
-    { value: 'Automated Testing', label: 'Automated Testing' },
-    { value: 'Code Merging', label: 'Code Merging' }
+    DEVELOPMENT: [
+        { value: 'Development', label: 'Development' },
+        { value: 'Meeting', label: 'Meeting' },
+        { value: 'Leading', label: 'Leading' },
+        { value: 'Learning', label: 'Learning' },
+        { value: 'Traning', label: 'Traning' },
+        { value: 'Code Review', label: 'Code Review' },
+        { value: 'Code Analysis', label: 'Code Analysis' },
+        { value: 'Code Analysis', label: 'Code Analysis' },
+        { value: 'Code Refactoring', label: 'Code Refactoring' },
+        { value: 'Session', label: 'Session' },
+        { value: 'Discussion', label: 'Discussion' },
+        { value: 'Debugging', label: 'Debugging' },
+        { value: 'Reporting', label: 'Reporting' },
+        { value: 'Writing Docs', label: 'Writing Docs' },
+        { value: 'Designing', label: 'Designing' },
+        { value: 'Project Setup', label: 'Project Setup' },
+        { value: 'Code Merging', label: 'Code Merging' },
+        { value: 'Automated Testing', label: 'Automated Testing' },
+        { value: 'Unit Testing', label: 'Unit Testing' },
+        { value: 'Integration Testing', label: 'Integration Testing' },
+
+
     ],
     HR: [
         { value: 'Emailing', label: 'Emailing' },
@@ -30,6 +33,15 @@ const _tasks = {
         { value: 'Resume Screening', label: 'Resume Screening' },
         { value: 'Meeting', label: 'Meeting' },
         { value: 'Job Posting', label: 'Job Posting' },
-    ]
+    ],
+    Business:
+        [
+            { value: 'Upwork Proposal', label: 'Upwork Proposal' },
+            { value: 'Glassdoor', label: 'Glassdoor' },
+            { value: 'Documentation', label: 'Documentation' },
+            { value: 'Connections', label: 'Connections' },
+
+        ]
+
 }
 export default _tasks
